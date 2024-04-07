65c381a5a7b74fdc4c707606a648f21669a4c56e
scalifycommerce <165213165+scalifycommerce@users.noreply.github.com>
2024-04-08 00:25:17 +0600

    next js package

---
+++ app/update/page.jsx
@@ -0,0 +1,11 @@
+import ReleaseOfUpdate from '../components/ReleaseOfUpdate';
+
+const page = () => {
+    return (
+        <div>
+            <ReleaseOfUpdate/>
+        </div>
+    );
+};
+
+export default page;
